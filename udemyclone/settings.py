0130c2a7eec9f02e110310188b3f8d58ad7c42d7
w-root <feneremree553@gmail.com>
2022-09-10 21:06:49 +0300

    Heroku deployment

--- udemyclone/settings.py
+++ udemyclone/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,4 +160,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CSRF_TRUSTED_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = ['*']
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
+django_heroku.settings(locals())
